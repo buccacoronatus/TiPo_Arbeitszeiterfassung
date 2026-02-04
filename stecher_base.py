3c0bf2e388066a3c9e7612332c69c020373228be
buccacoronatus <stefan.beckert@googlemail.com>
2026-02-04 10:47:30 +0100

    korrektur

--- stecher_base.py
+++ stecher_base.py
@@ -11,18 +11,16 @@ class Time_Poker():
     def pause():
         pass
     
-    def calculate_stunden(self, end):
-        return round(round((end - self.start) / 3600, 1) * 2) / 2
+    def calculate_stunden(self):
+        return round(round((time.time() - self.start) / 3600, 1) * 2) / 2
     
-    def write_to_csv(self, stunden):
-        csv_string = f'{self.Wochentag}, {self.Jahr}, {self.Monat}, {self.Tag}, {stunden}\n'
+    def write_to_csv(self):
+        csv_string = f'{self.Wochentag}, {self.Jahr}, {self.Monat}, {self.Tag}, {timer.calculate_stunden()}\n'
         print(csv_string, sep="")
 
         with open('.//test_data.csv', 'a', encoding="UTF-8") as data_file:
             data_file.write(csv_string)
 
 timer = Time_Poker()
-
 input('Taste drücken zum beenden und speichern')
-
-timer.write_to_csv(timer.calculate_stunden(time.time()))
+timer.write_to_csv()
