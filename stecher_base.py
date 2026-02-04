e4382e1eb5e420d4e1131d57da56008af6169226
buccacoronatus <stefan.beckert@googlemail.com>
2026-02-04 09:31:04 +0100

    Spaghetti in Objekt

--- stecher_base.py
+++ stecher_base.py
@@ -1,29 +1,32 @@
 import time
 
-# Pauseknopf um Pause zu ermitteln 
-# exportknopf um csv zu übertragen 
+class Time_Poker():
+    def __init__(self):
+        self.Jahr = time.localtime()[0]
+        self.Monat = time.localtime()[1]
+        self.Tag = time.localtime()[2]
+        self.Wochentag = ['Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa', 'So'][time.localtime()[6]]
 
-heute= {
-    'Jahr': time.localtime()[0],
-    'Monat': time.localtime()[1],
-    'Tag': time.localtime()[2],
-    'Wochentag': ['Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa', 'So'][time.localtime()[6]]
-}
+    def take_time(self):
+        return time.time()
 
-start = time.time()
+    def pause():
+        pass
 
+    def calculate_stunden(start, end):
+        return round(round((end - start) / 3600, 1) * 2) / 2
+    
+    def write_to_csv(self, start, end):
+        print(start, end)
+        stunden = round(round((end - start) / 3600, 1) * 2) / 2
+        csv_string = f'{self.Wochentag}, {self.Jahr}, {self.Monat}, {self.Tag}, {stunden}\n'
+        print(csv_string, sep="")
+
+        with open('.//test_data.csv', 'a', encoding="UTF-8") as data_file:
+            data_file.write(csv_string)
+
+timer = Time_Poker()
+start = timer.take_time()
 input('Taste drücken zum beenden')
-# testweise Zahl eingefügt!
-end = time.time()
 
-# aufgerundet auf halbe Stunde 
-stunden = round(round((end - start) / 3600, 1) * 2) / 2
-
-
-# csv print, sep="" wichtig um whitespace zu vermeiden
-csv_string = f'{heute['Wochentag']}, {heute['Jahr']}, {heute['Monat']}, {heute['Tag']}, {stunden}\n'
-print(csv_string, sep="")
-
-# a for append
-with open('.//data.csv', 'a', encoding="UTF-8") as data_file:
-    data_file.write(csv_string)
+timer.write_to_csv(start, timer.take_time())
