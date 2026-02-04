861b91a5e38116056167d3370803ee4f20a0abb3
buccacoronatus <stefan.beckert@googlemail.com>
2026-02-04 11:54:59 +0100

    added csv handling

--- stecher_base.py
+++ stecher_base.py
@@ -1,11 +1,16 @@
 import time
+import csv
+
 
 class TimePoker():
     def __init__(self):
-        self.jahr = time.localtime()[0]
-        self.monat = time.localtime()[1]
-        self.tag = time.localtime()[2]
-        self.wochentag = ['Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa', 'So'][time.localtime()[6]]
+        time_object = time.localtime()
+        
+        self.jahr = time_object[0]
+        self.monat = time_object[1]
+        self.tag = time_object[2]
+        self.wochentag = ['Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa', 'So'][time_object[6]]
+        
         self.start = time.time()
 
     def run(self):
@@ -21,11 +26,13 @@ class TimePoker():
     def write_to_csv(self):
         csv_string = f'{self.wochentag}, {self.jahr}, {self.monat}, {self.tag}, {self.calculate_stunden()}\n'
         print(csv_string, sep="")
-
-        with open('.//test_data.csv', 'a', encoding="UTF-8") as data_file:
-            data_file.write(csv_string, sep="")
+        
+        with open('test_data.csv', 'a', newline='') as csvfile:
+            csv.writer(
+                csvfile, delimiter=' ').writerow(
+                [self.wochentag]+[self.jahr]+[self.monat]+[self.tag]+[self.calculate_stunden()]
+                )
 
 timer = TimePoker()
 timer.run()
 
-# csv.writer
